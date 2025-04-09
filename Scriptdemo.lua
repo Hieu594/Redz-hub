e1486da501797779b8f12f21b2f3469185007a92
Hieu594 <tranminhhieu2647@gmail.com>
2025-04-09 13:01:27 +0700

    Add files via upload

---
+++ Scriptdemo.lua
@@ -0,0 +1,31 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Hieu Redz Hub",
+         Animation = "Redz Hub"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=83190276951914",
+       Size = {30, 30},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
